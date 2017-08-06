337eb5c59d0e65aea30b95799b163e391a042598
Andrewmat <andre.m.santos1@gmail.com>
2017-08-06 17:20:54 -0300

    Redoing last commit

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import FotoItem from './FotoItem';
 
 export default class Timeline extends Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: []
+    };
+  }
+  
+  componentDidMount() {
+    fetch('http://localhost:8080/api/public/fotos/alots')
+      .then(result => result.json())
+      .then(photos => {
+        this.setState({ photos });
+      })
+      ;
+  }
 
   render() {
     return (
       <div className="fotos container">
+        {
+          this.state.photos.map(p => (
+            <FotoItem fotoUrl={p} />
+          ))
+        }
         <FotoItem />
         <FotoItem />
       </div>
