9c042897996b77daaa40ff4de33860c0807c1521
Andrewmat <andre.m.santos1@gmail.com>
2017-08-08 01:53:04 -0300

    Full navigation

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -7,16 +7,39 @@ export default class Timeline extends Component {
     this.state = {
       photos: []
     };
+    this.user = '';
   }
 
-  componentDidMount() {
-    fetch(`http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
+  fetchTimelineData() {
+    let endpoint;
+    if (this.user) {
+      endpoint = `http://localhost:8080/api/public/fotos/${this.user}`;
+    } else {
+      endpoint = `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
+    }
+    fetch(endpoint)
       .then(result => {
         return result.json();
       })
       .then(photos => {
         this.setState({ photos });
       });
+  }
+
+  componentDidMount() {
+    if (this.props.match && this.props.match.params && this.props.match.params.user) {
+      console.log(2);
+      this.user = this.props.match.params.user;
+    }
+    this.fetchTimelineData();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let nextUser = nextProps.match && nextProps.match.params && nextProps.match.params.user
+    if (nextUser == null || nextUser !== this.user) {
+      this.user = nextUser || '';
+      this.fetchTimelineData();
+    }
   }
 
   render() {
